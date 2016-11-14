9ae5ffd5429ab060f076161ad7090d7b4a3c08e3
wanghao <hcwanghao@gmail.com>
2016-11-14 12:45:21 +0800

    new input

--- src/data_input.py
+++ src/data_input.py
@@ -20,18 +20,18 @@ axis = [0 for _ in range(0, len(storage[0]))]
 for j in range(3, len(storage[0])):
     axis[j] = axis[j-1] + category[j-1]
 
-wfile = open('/home/wanghao/workspace/algorithm_lab1/code/data/input_log.csv', 'w')
+wfile = open('/home/wanghao/workspace/algorithm_lab1/code/data/data.txt.train', 'w')
 
 print(category)
 
 for line in storage[1:]:
-    wfile.write(line[1] + ' ')
+    wfile.write('%s '%line[0])
     for j in range(2, len(storage[0])):
         if line[j][0] >= 'a' and line[j][0] <= 'z':
             temp = ord(line[j][0]) - ord('a')
             wfile.write('%d:1 '%(temp+axis[j]))
         else:
-            wfile.write('%d:%.4f '%(axis[j], math.log(int(line[j])+1)))
+            wfile.write('%d:%s '%(axis[j], line[j]))
     wfile.write('\n')
 
 wfile.close()
