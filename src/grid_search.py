d019283a16d780d537fa1c5f4a6f44cdc4006dff
EastonWang <eastonwyz@gmail.com>
2016-11-22 15:05:53 +0800

    write our own grid search

--- src/grid_search.py
+++ src/grid_search.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 import pandas as pd
 import xgboost as xgb
-from sklearn.grid_search import GridSearchCV
+import itertools
 
 
 train_data = pd.read_csv('../data/train.csv')
@@ -18,28 +18,41 @@ all_X = all_X / all_X.max()
 # re-split the data into train and test
 train_X = all_X[:train_data.shape[0]]
 test_X = all_X[train_data.shape[0]:]
-# hyper-parameters that need cross validation
-cv_params = {'max_depth': [3, 5],
-             'min_child_weight': [1, 3],
-             'objective': ['reg:linear', 'count:poisson']}
-# fixed hyper-parameters
-fixed_params = {'learning_rate': 0.1,
-                'n_estimators': 1000,
-                'gamma': 0,
-                'nthread': -1,
-                'seed': 0,
-                'subsample':1}
-# use grid search to find the best classifier
-best_clf = GridSearchCV(xgb.XGBRegressor(**fixed_params), cv_params, scoring='mean_squared_error', cv=2, n_jobs=-1, verbose=10)
-best_clf.fit(train_X, train_y)
-# log information
-print('Grid sores are:')
-print(best_clf.grid_scores_)
-print('We are now re-train the model using the best hyperparameters: {}'.format(best_clf.best_params_))
-final_clf = xgb.XGBRegressor(**best_clf.best_params_)
-final_clf.fit(train_X, train_y)
+dtrain = xgb.DMatrix(train_X, train_y)
+dtest = xgb.DMatrix(test_X)
+# use grid search to find the best hyperperameters
+param_grid = {
+    'n_estimators': [1000],
+    'learning_rate': [0.05, 0.1, 0.2],
+    'max_depth': [3, 5, 7, 10],
+    'min_child_weight': [1, 3, 5],
+    'subsample': [1, 0.7],
+    'objective': ['reg:linear', 'count:poisson'],
+    'silent': [True]
+}
+# create all possible hyperperameter groups
+param_groups = itertools.product(*param_grid.values())
+
+best_booster_params = None
+for param_group in param_groups:
+    booster_params = {name: value for name, value in zip(param_grid.keys(), param_group)}
+    print('Now we are training and validating hyperperameters: {}.'.format(booster_params))
+    result = xgb.cv(params=booster_params, num_boost_round=500, early_stopping_rounds=1, dtrain=dtrain, nfold=3, metrics='rmse')
+    print('The cross validation result for each boosting round is: \n{}'.format(result))
+    last_iter_result = result.iloc[-1]
+    num_boost_round = last_iter_result.name
+    rmse = last_iter_result['test-rmse-mean']
+    if best_booster_params is None or rmse < best_booster_params['rmse']:
+        best_booster_params = {'booster_params': booster_params, 'num_boost_round': num_boost_round, 'rmse': rmse}
+
+print('Best parameters are: {} with num_boost_round as {} and rmse as {}'.format(best_booster_params['booster_params'],
+                                                                                 best_booster_params['num_boost_round'],
+                                                                                 best_booster_params['rmse']))
+# train the final model using the best hyperperameters
+final_model = xgb.train(params=best_booster_params['booster_params'], dtrain=dtrain, num_boost_round=best_booster_params['num_boost_round'])
+
 # predict
-pred_y = final_clf.predict(test_X)
+pred_y = final_model.predict(dtest)
 # output
 with open('../data/predict.txt', 'w') as fout:
     for y in pred_y:
